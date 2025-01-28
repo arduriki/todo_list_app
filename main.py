4378616f10c8f5af696c3d210264c787b8250693
arduriki <jordi.ardura.professional@gmail.com>
2025-01-28 21:51:30 +0100

    learning f-strings

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ while True:
     # .strip()
     # if the user adds a whitespace in the command
     # the program will still be able to know the command
-    user_action = input("Type add, show or exit: ").strip()
+    user_action = input("Type add, show, edit or exit: ").strip()
 
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
-            for todo in todos:
-                print(todo)
+            for index, item in enumerate(todos):
+                print(f"{index+1}-{item}")
+        case "edit":
+            number = int(input("Number of the todo to edit: ")) - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case "exit":
             break
-        case _:
-            print("Sorry, enter a valid command")
 
 print("Bye!")
