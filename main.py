022fcb378b307fa8ee40a00e730445fabc0e5855
arduriki <jordi.ardura.professional@gmail.com>
2025-02-03 18:02:20 +0100

    case 'complete' added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ while True:
     # .strip()
     # if the user adds a whitespace in the command
     # the program will still be able to know the command
-    user_action = input("Type add, show, edit or exit: ").strip()
+    user_action = input("Type add, show, edit, complete or exit: ").strip()
 
     match user_action:
         case "add":
@@ -12,11 +12,14 @@ while True:
             todos.append(todo)
         case "show":
             for index, item in enumerate(todos):
-                print(f"{index+1}-{item}")
+                print(f"{index + 1}-{item}")
         case "edit":
             number = int(input("Number of the todo to edit: ")) - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case "complete":
+            number = int(input("Number of the todo to complete: ")) - 1
+            todos.pop(number)
         case "exit":
             break
 
